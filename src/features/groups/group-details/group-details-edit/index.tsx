1c1894be2d2eb83c5441fb3e5578638fb5a53407
max21dev <max21dev@gmail.com>
2024-09-10 23:31:45 +0200

    add confirm dialog on group metadata update

--- src/features/groups/group-details/group-details-edit/index.tsx
+++ src/features/groups/group-details/group-details-edit/index.tsx
@@ -1,5 +1,4 @@
-import { Dispatch, SetStateAction } from 'react';
-
+import { Dispatch, SetStateAction, useState } from 'react';
 import { Button } from '@/shared/components/ui/button';
 import {
   Form,
@@ -11,10 +10,16 @@ import {
   FormMessage,
 } from '@/shared/components/ui/form';
 import { Input } from '@/shared/components/ui/input';
-import { Textarea } from '@/shared/components/ui/textarea.tsx';
+import { Textarea } from '@/shared/components/ui/textarea';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+} from '@/shared/components/ui/dialog';
 
 import { Group } from '@/shared/types';
-
 import { useGroupDetailsEdit } from './hooks';
 
 type Props = {
@@ -24,53 +29,82 @@ type Props = {
 
 export const GroupDetailsEdit = ({ group, setEditMode }: Props) => {
   const { form, onSubmit } = useGroupDetailsEdit({ group, setEditMode });
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setIsDialogOpen(true);
+  };
+
+  const handleConfirm = () => {
+    form.handleSubmit(onSubmit)();
+    setIsDialogOpen(false);
+  };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 mt-6">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Group name</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter Group Name" {...field} />
-              </FormControl>
-              <FormDescription>Enter the name of the group.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="picture"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Picture</FormLabel>
-              <FormControl>
-                <Input placeholder="Picture URL" {...field} />
-              </FormControl>
-              <FormDescription>Enter the picture URL of the group.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="about"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>about</FormLabel>
-              <FormControl>
-                <Textarea placeholder="About group ..." {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <>
+      <Form {...form}>
+        <form onSubmit={handleSubmit} className="space-y-2 mt-6">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Group name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Group Name" {...field} />
+                </FormControl>
+                <FormDescription>Enter the name of the group.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="picture"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Picture</FormLabel>
+                <FormControl>
+                  <Input placeholder="Picture URL" {...field} />
+                </FormControl>
+                <FormDescription>Enter the picture URL of the group.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="about"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>About</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="About group ..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+
+      {/* Confirmation Dialog */}
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Confirm</DialogTitle>
+          </DialogHeader>
+          <p>Are you sure you want to update group metadata?</p>
+          <DialogFooter>
+            <Button variant="outline" onClick={() => setIsDialogOpen(false)}>
+              Cancel
+            </Button>
+            <Button onClick={handleConfirm}>Confirm</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
