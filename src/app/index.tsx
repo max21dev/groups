24f977b898f01b4e01f8697a9510e0390f9b9efe
max21dev <max21dev@gmail.com>
2024-09-27 23:34:51 +0200

    Update app initialization

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,32 +5,34 @@ import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from '@/pages';
-
 import { ThemeProvider } from '@/shared/components/theme-provider';
 import { Toaster } from '@/shared/components/ui/toaster';
-
 import { useGlobalNdk, useNip29Ndk } from '@/shared/hooks';
 import { useStore } from '@/shared/store';
 
 import './index.css';
 
 export const App = () => {
-  const relays = useStore((state) => state.relays);
-  const activeRelayIndex = useStore((state) => state.activeRelayIndex);
+  const { relays, setRelayStatus, activeRelayUrl } = useStore((state) => ({
+    relays: state.relays,
+    setRelayStatus: state.setRelayStatus,
+    activeRelayUrl: state.activeRelayUrl,
+  }));
 
   const { globalNdk, setGlobalNdk } = useGlobalNdk();
   const { nip29Ndk, setNip29Ndk } = useNip29Ndk();
 
-  const { setSigner: setGlobalSigner } = useSigner({
-    customNdk: globalNdk,
-    setCustomNdk: setGlobalNdk,
-  });
-  const { setSigner: setNip29Signer } = useSigner({
-    customNdk: nip29Ndk,
-    setCustomNdk: setNip29Ndk,
-  });
-
+  const { setSigner: setGlobalSigner } = useSigner({ customNdk: globalNdk, setCustomNdk: setGlobalNdk });
+  const { setSigner: setNip29Signer } = useSigner({ customNdk: nip29Ndk, setCustomNdk: setNip29Ndk });
   const { loginFromLocalStorage } = useLogin({ customNdk: globalNdk, setCustomNdk: setGlobalNdk });
+
+  const removeTrailingSlash = (url: string): string => url.endsWith('/') ? url.slice(0, -1) : url;
+
+  const handleRelayStatus = (status: string) => (relay: { url: string }) => {
+    const relayUrl = removeTrailingSlash(relay.url);
+    relays.find((r) => r.url === relayUrl) ?? setRelayStatus(relayUrl, status);
+    console.log(`${status} to relay`, relayUrl);
+  };
 
   useEffect(() => {
     globalNdk.connect();
@@ -38,22 +40,29 @@ export const App = () => {
 
   useEffect(() => {
     nip29Ndk.connect();
-  }, [nip29Ndk]);
+
+    const pool = nip29Ndk.pool;
+    if (pool) {
+      pool.on('relay:connecting', handleRelayStatus('CONNECTING'));
+      pool.on('relay:connect', handleRelayStatus('CONNECTED'));
+      pool.on('relay:disconnect', handleRelayStatus('DISCONNECTED'));
+    }
+
+  }, [nip29Ndk, relays]);
 
   useEffect(() => {
     const ndk = new NDK({
-      explicitRelayUrls: [relays[activeRelayIndex]],
+      explicitRelayUrls: [activeRelayUrl],
       autoConnectUserRelays: false,
       autoFetchUserMutelist: false,
-      cacheAdapter: new NDKCacheAdapterDexie({ dbName: `db-${relays[activeRelayIndex]}` }),
+      cacheAdapter: new NDKCacheAdapterDexie({ dbName: `db-${activeRelayUrl}` }),
       signer: globalNdk.signer,
       relayAuthDefaultPolicy: undefined, // Placeholder for now, we'll set it after initialization
     });
 
     ndk.relayAuthDefaultPolicy = NDKRelayAuthPolicies.signIn({ ndk, signer: globalNdk.signer });
-
     setNip29Ndk(ndk);
-  }, [relays, activeRelayIndex, globalNdk.signer]);
+  }, [activeRelayUrl, globalNdk.signer, setNip29Ndk]);
 
   useEffect(() => {
     loginFromLocalStorage({
@@ -65,11 +74,9 @@ export const App = () => {
   }, [loginFromLocalStorage, setGlobalSigner, setNip29Signer]);
 
   return (
-    <>
-      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <RouterProvider router={router} />
-        <Toaster />
-      </ThemeProvider>
-    </>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+      <Toaster />
+    </ThemeProvider>
   );
 };
