ecb2725bc481bebb7e3a009af2a54bd1a14d6437
max21dev <max21dev@gmail.com>
2024-10-05 21:10:12 +0200

    add copy-to-clipboard functionality for edit group members

--- src/features/groups/group-details/group-details-edit/hooks/membersColumns.tsx
+++ src/features/groups/group-details/group-details-edit/hooks/membersColumns.tsx
@@ -9,7 +9,8 @@ import {
 } from '@/shared/components/ui/tooltip.tsx';
 import { useGlobalProfile } from '@/shared/hooks';
 import { Button } from '@/shared/components/ui/button.tsx';
-import { Trash } from 'lucide-react';
+import { Check, Copy, Trash } from 'lucide-react';
+import { useState } from 'react';
 
 export const membersColumns: ColumnDef<GroupMember>[] = [
   {
@@ -50,10 +51,46 @@ export const membersColumns: ColumnDef<GroupMember>[] = [
       return profile?.name ? profile?.name : '-';
     },
   },
+
   {
     id: 'publicKey',
     accessorKey: 'publicKey',
     header: 'Public Key',
+    cell: ({ row }) => {
+      const pubkey: string = row?.getValue('publicKey');
+      const shortenedPubkey = pubkey ? `${pubkey.slice(0, 10)}...${pubkey.slice(-10)}` : '';
+
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const [copied, setCopied] = useState(false);
+
+      const handleCopy = () => {
+        if (pubkey) {
+          navigator.clipboard.writeText(pubkey);
+          setCopied(true);
+          setTimeout(() => setCopied(false), 2000); // Reset the copied state after 2 seconds
+        }
+      };
+
+      return (
+        <TooltipProvider>
+          <div className="flex items-center space-x-2">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <span>{shortenedPubkey}</span>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{pubkey}</p>
+              </TooltipContent>
+            </Tooltip>
+
+            {/* Copy to clipboard button */}
+            <Button variant="ghost" size="sm" onClick={handleCopy}>
+              {copied ? <Check className="w-4 h-4 text-green-500" /> : <Copy className="w-4 h-4" />}
+            </Button>
+          </div>
+        </TooltipProvider>
+      );
+    },
   },
   {
     id: 'actions',
