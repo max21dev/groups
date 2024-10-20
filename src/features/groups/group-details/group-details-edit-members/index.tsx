b30ff14af5962e9441e4f3e9b8ba16a50c7ee80d
max21dev <max21dev@gmail.com>
2024-10-21 00:09:50 +0200

    Add 'add permission dialog' for user tab

--- src/features/groups/group-details/group-details-edit-members/index.tsx
+++ src/features/groups/group-details/group-details-edit-members/index.tsx
@@ -2,16 +2,26 @@ import { useActiveGroup, useGroupMembers } from '@/shared/hooks';
 import { DataTable } from '@/features/groups/group-details/group-details-edit/hooks/data-table.tsx';
 import { membersColumns } from '@/features/groups/group-details/group-details-edit/hooks/membersColumns.tsx';
 import { useGroupDetails } from '@/features/groups/group-details/hooks';
+import { GroupAdminPermission } from '@/shared/types';
 
 export const GroupDetailsEditMembers = () => {
   const { activeGroupId } = useActiveGroup();
   const { members } = useGroupMembers(activeGroupId);
-  const { handleRemoveUserFromGroup } = useGroupDetails({ groupId: activeGroupId });
+  const { handleRemoveUserFromGroup, handleAddAdminPermissions } = useGroupDetails({
+    groupId: activeGroupId,
+  });
 
   const removeMember = (pubkey: string) => {
     handleRemoveUserFromGroup(pubkey);
   };
-  const columns = membersColumns(removeMember);
+
+  const addUserPermissions = (pubkey: string, newPermissions: GroupAdminPermission[] | []) => {
+    if (newPermissions.length > 0) {
+      handleAddAdminPermissions(pubkey, newPermissions);
+    }
+  };
+
+  const columns = membersColumns(removeMember, addUserPermissions);
 
   return <div>{members && <DataTable columns={columns} data={members} />}</div>;
 };
--- src/features/groups/group-details/group-details-edit/hooks/membersColumns.tsx
+++ src/features/groups/group-details/group-details-edit/hooks/membersColumns.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { ColumnDef } from '@tanstack/react-table';
-import { GroupMember } from '@/shared/types';
+import { GroupAdminAvailablePermission, GroupAdminPermission, GroupMember } from '@/shared/types';
 import { Avatar, AvatarFallback, AvatarImage } from '@/shared/components/ui/avatar';
 import {
   Tooltip,
@@ -9,13 +9,15 @@ import {
   TooltipTrigger,
 } from '@/shared/components/ui/tooltip';
 import { Button } from '@/shared/components/ui/button';
-import { Check, Copy, Trash } from 'lucide-react';
+import { Check, Copy, Edit, Trash } from 'lucide-react';
 import { useGlobalProfile } from '@/shared/hooks';
 import { ConfirmDialog } from '@/features/groups/group-details/confirm-dialog';
+import { Checkbox } from '@/shared/components/ui/checkbox.tsx';
 
 export const membersColumns: (
   removeMember: (pubkey: string) => void,
-) => ColumnDef<GroupMember>[] = (removeMember) => [
+  addUserPermissions: (pubkey: string, newPermissions: GroupAdminPermission[] | []) => void,
+) => ColumnDef<GroupMember>[] = (removeMember, addUserPermissions) => [
   {
     id: 'avatar',
     header: 'Avatar',
@@ -99,31 +101,83 @@ export const membersColumns: (
     cell: ({ row }) => {
       const pubkey: string = row?.getValue('publicKey');
       // eslint-disable-next-line react-hooks/rules-of-hooks
-      const [isDialogOpen, setDialogOpen] = useState(false);
+      const [isSetRolesDialogOpen, setSetRolesDialogOpen] = useState(false); // Separate state for Set Roles
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const [isRemoveDialogOpen, setRemoveDialogOpen] = useState(false); // Separate state for Remove Dialog
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const [permissions, setPermissions] = useState<GroupAdminPermission[]>([]); // State for permissions
+
+      // Handle checkbox change
+      const handleCheckboxChange = (permission: GroupAdminPermission) => {
+        if (permissions.includes(permission)) {
+          // If the permission is already included, remove it
+          setPermissions(permissions.filter((p) => p !== permission));
+        } else {
+          // Otherwise, add it
+          setPermissions([...permissions, permission]);
+        }
+      };
 
       const handleRemove = () => {
         removeMember(pubkey);
-        setDialogOpen(false);
+        setRemoveDialogOpen(false);
+      };
+
+      const handleAddPermissions = () => {
+        addUserPermissions(pubkey, permissions);
+        setSetRolesDialogOpen(false);
       };
 
       return (
-        <ConfirmDialog
-          triggerButton={
-            <Button variant="destructive">
-              <Trash className="h-3 w-3 mr-2" />
-              Remove
-            </Button>
-          }
-          title="Confirm Removal"
-          confirmButtonLabel="Remove"
-          confirmButtonVariant="destructive"
-          confirmAction={handleRemove}
-          open={isDialogOpen}
-          onOpenChange={setDialogOpen}
-        >
-          <p>Are you sure you want to remove this member? This action cannot be undone.</p>
-          <p>{String(row?.getValue('publicKey'))?.slice(0, 10) ?? '-'}...</p>
-        </ConfirmDialog>
+        <div>
+          <ConfirmDialog
+            triggerButton={
+              <Button variant="default" className="mb-2">
+                <Edit className="h-3 w-3 mr-2" />
+                Set Roles
+              </Button>
+            }
+            title="Add Permissions"
+            confirmButtonLabel="Add Permissions"
+            confirmButtonVariant="default"
+            confirmAction={handleAddPermissions}
+            open={isSetRolesDialogOpen}
+            onOpenChange={setSetRolesDialogOpen}
+          >
+            <p>Change Permission for this user</p>
+            <p>{String(row?.getValue('publicKey'))?.slice(0, 10) ?? '-'}...</p>
+            <p>
+              {GroupAdminAvailablePermission.map((permission: GroupAdminPermission) => (
+                <div className="m-2" key={permission}>
+                  <Checkbox
+                    checked={permissions.includes(permission)}
+                    onCheckedChange={() => handleCheckboxChange(permission)} // Handle state change
+                    id={permission}
+                  />
+                  <span className="ml-2"> {permission}</span>
+                </div>
+              ))}
+            </p>
+          </ConfirmDialog>
+
+          <ConfirmDialog
+            triggerButton={
+              <Button variant="destructive">
+                <Trash className="h-3 w-3 mr-2" />
+                Remove
+              </Button>
+            }
+            title="Confirm Removal"
+            confirmButtonLabel="Remove"
+            confirmButtonVariant="destructive"
+            confirmAction={handleRemove}
+            open={isRemoveDialogOpen}
+            onOpenChange={setRemoveDialogOpen}
+          >
+            <p>Are you sure you want to remove this member? This action cannot be undone.</p>
+            <p>{String(row?.getValue('publicKey'))?.slice(0, 10) ?? '-'}...</p>
+          </ConfirmDialog>
+        </div>
       );
     },
   },
