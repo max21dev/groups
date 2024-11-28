d6d54d41fa1755a22180dab91f044710e7b8b5a4
max21dev <max21dev@gmail.com>
2024-11-28 09:42:12 +0330

    update with new nostr-hooks, remove all nip29 hooks

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,16 +1,6 @@
 export { useActiveGroup } from './use-active-group';
 export { useActiveRelay } from './use-active-relay';
 export { useCopyToClipboard } from './use-copy-to-clipboard';
-export { useGlobalNdk } from './use-global-ndk';
 export { useGlobalProfile } from './use-global-profile';
-export { useGroup } from './use-group';
-export { useGroupAdmin } from './use-group-admin';
-export { useGroupAdmins } from './use-group-admins';
-export { useGroupMembers } from './use-group-members';
-export { useGroupMessages } from './use-group-messages';
-export { useGroupRoles } from './use-group-roles';
-export { useGroups } from './use-groups';
 export { useLoginModalState } from './use-login-modal-state';
-export { useMessageReactions } from './use-message-reactions';
-export { useNip29Ndk } from './use-nip29-ndk';
 export { useZapModalState } from './use-zap-modal-state';
