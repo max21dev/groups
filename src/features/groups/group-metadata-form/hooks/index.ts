b5c56cb8f704957175f5ed101158b8c94697f721
max21dev <max21dev@gmail.com>
2024-12-05 22:58:56 +0100

    Adding upload image for group picture

---
+++ src/features/groups/group-metadata-form/hooks/index.ts
@@ -0,0 +1,75 @@
+import { useRef, useState } from 'react';
+
+import { useToast } from '@/shared/components/ui/use-toast';
+
+export const useGroupMetadataForm = (setPicture: (url: string) => void) => {
+  const groupImageUrlInputRef = useRef<HTMLInputElement>(null);
+  const [isUploadingGroupImage, setIsUploadingGroupImage] = useState(false);
+
+  const { toast } = useToast();
+
+  const updateGroupImageUrl = (url: string) => {
+    setPicture(url);
+    if (groupImageUrlInputRef.current) {
+      groupImageUrlInputRef.current.focus();
+    }
+  };
+
+  const handleUploadError = (e: unknown) => {
+    console.error(e);
+    toast({ title: 'Error', description: 'Failed to upload Group Image', variant: 'destructive' });
+  };
+
+  const openUploadImageDialog = () => {
+    const input = document.createElement('input');
+    const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+    input.type = 'file';
+    input.accept = allowedTypes.join(',');
+
+    input.onchange = (event) => {
+      const file = (event.target as HTMLInputElement).files?.[0];
+      if (!file) return;
+
+      if (!allowedTypes.includes(file.type)) {
+        toast({
+          title: 'Error',
+          description: 'Invalid file type. Please upload an image.',
+          variant: 'destructive',
+        });
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append('fileToUpload', file);
+
+      setIsUploadingGroupImage(true);
+
+      fetch(import.meta.env.VITE_NOSTR_BUILD_UPLOAD_API_ENDPOINT, {
+        method: 'POST',
+        body: formData,
+      })
+        .then((res) => res.json())
+        .then(({ status, data }) => {
+          if (status === 'success' && data?.[0]?.url) {
+            updateGroupImageUrl(data[0].url);
+          } else {
+            handleUploadError(status);
+          }
+        })
+        .catch((e) => {
+          handleUploadError(e);
+        })
+        .finally(() => {
+          setIsUploadingGroupImage(false);
+        });
+    };
+
+    input.click();
+  };
+
+  return {
+    groupImageUrlInputRef,
+    isUploadingGroupImage,
+    openUploadImageDialog,
+  };
+};
--- src/features/groups/group-metadata-form/index.tsx
+++ src/features/groups/group-metadata-form/index.tsx
@@ -1,11 +1,13 @@
 import { EyeClosedIcon, EyeIcon, LockIcon, LockOpenIcon } from 'lucide-react';
 import { editGroupMetadata, Nip29GroupMetadata } from 'nostr-hooks/nip29';
 import { useCallback, useState } from 'react';
-
+import { UploadImageButton } from '@/features/chats/chat-bottom-bar/components';
 import { Button } from '@/shared/components/ui/button';
 import { Input } from '@/shared/components/ui/input';
 import { Label } from '@/shared/components/ui/label';
+import { Textarea } from '@/shared/components/ui/textarea';
 import { useToast } from '@/shared/components/ui/use-toast';
+import { useGroupMetadataForm } from './hooks';
 
 export type GroupMetadataFormProps = {
   relay: string | undefined;
@@ -19,9 +21,9 @@ export const GroupMetadataForm = ({ initialMetadata, relay, groupId }: GroupMeta
   const [about, setAbout] = useState(initialMetadata?.about ?? '');
   const [isPublic, setIsPublic] = useState(initialMetadata?.isPublic ?? true);
   const [isOpen, setIsOpen] = useState(initialMetadata?.isOpen ?? true);
-
   const { toast } = useToast();
-
+  const { groupImageUrlInputRef, openUploadImageDialog, isUploadingGroupImage } =
+    useGroupMetadataForm(setPicture);
   const handleSubmit = useCallback(() => {
     relay &&
       groupId &&
@@ -58,22 +60,31 @@ export const GroupMetadataForm = ({ initialMetadata, relay, groupId }: GroupMeta
 
         <div>
           <Label htmlFor="group-picture">Picture (URL):</Label>
-          <Input
-            id="group-picture"
-            type="text"
-            placeholder="https://example.com/picture.jpg"
-            value={picture}
-            onChange={(e) => setPicture(e.target.value)}
-          />
+          <div className="relative">
+            <Input
+              ref={groupImageUrlInputRef}
+              id="group-picture"
+              className="pr-10"
+              type="text"
+              placeholder="https://example.com/picture.jpg"
+              value={picture}
+              onChange={(e) => setPicture(e.target.value)}
+            />
+            <span className="[&_.animate-spin]:h-6 [&_.animate-spin]:w-6 bg-gray-200 dark:bg-gray-900 rounded-md rounded-s-none absolute top-0 end-0">
+              <UploadImageButton
+                isUploadingMedia={isUploadingGroupImage}
+                openUploadMediaDialog={openUploadImageDialog}
+              />
+            </span>
+          </div>
         </div>
       </div>
 
       <div className="grid grid-cols-1 gap-4">
         <div>
           <Label htmlFor="group-about">About:</Label>
-          <Input
+          <Textarea
             id="group-about"
-            type="text"
             placeholder="We are the cool folks!"
             value={about}
             onChange={(e) => setAbout(e.target.value)}
