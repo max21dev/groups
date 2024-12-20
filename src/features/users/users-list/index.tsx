a42fe4ffa41e88c18ce21b9158ca42452be6f249
max21dev <max21dev@gmail.com>
2024-12-20 11:13:18 +0100

    Add user list item

---
+++ src/features/users/users-list/index.tsx
@@ -0,0 +1,18 @@
+import { UsersListItem } from './components/users-list-item';
+
+export const UsersList = ({ tags }: { tags: string[][] }) => {
+  const title = tags.find((tag) => tag[0] === 'title');
+  const description = tags.find((tag) => tag[0] === 'description');
+  console.log(title);
+  return (
+    <div className="flex flex-col gap-1 p-2 rounded-md overflow-y-scroll max-h-80 [overflow-wrap: anywhere;] max-w-80">
+      <h4>{title?.[1]}</h4>
+      <p className="mb-1">{description?.[1]}</p>
+      {tags
+        .filter((tag) => tag[0] === 'p')
+        .map((tag, index) => (
+          <UsersListItem key={index} pubkey={tag[1]} />
+        ))}
+    </div>
+  );
+};
