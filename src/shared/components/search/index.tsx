cf2b8260e528505f484688aa7b347edd35d6bf60
max21dev <max21dev@gmail.com>
2025-01-04 10:12:11 +0100

    Add group Search Component to sider navbar in group list view

---
+++ src/shared/components/search/index.tsx
@@ -0,0 +1,37 @@
+import { SearchIcon, X } from 'lucide-react';
+import { useRef } from 'react';
+
+interface SearchProps {
+  searchTerm: string;
+  setSearchTerm: (key: string) => void;
+  placeholder?: string;
+}
+
+export const Search = ({ searchTerm, setSearchTerm, placeholder = 'Search...' }: SearchProps) => {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <div className="flex items-center justify-center relative">
+      <input
+        type="text"
+        ref={searchInputRef}
+        placeholder={placeholder}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="w-full p-2 px-10 text-sm border border-input bg-background shadow-sm rounded-lg"
+      />
+      <SearchIcon size={17} className="absolute left-3 text-gray-500" />
+      {searchTerm && (
+        <button
+          onClick={() => {
+            setSearchTerm('');
+            searchInputRef?.current?.focus();
+          }}
+          className="absolute right-3 text-gray-500"
+        >
+          <X size={17} />
+        </button>
+      )}
+    </div>
+  );
+};
