4c3477fe20e84061616f7c34e5b476d77842e0d0
max21dev <max21dev@gmail.com>
2025-01-21 12:13:10 +0100

    Refactor JoinRequestButton and LoginButton components to accept customizable props for size, variant, and text

--- src/features/chats/chat-bottom-bar/components/join-request-button/index.tsx
+++ src/features/chats/chat-bottom-bar/components/join-request-button/index.tsx
@@ -5,15 +5,21 @@ import { useJoinRequestButton } from './hooks';
 export const JoinRequestButton = ({
   relay,
   groupId,
+  size = 'lg',
+  variant = 'default',
+  text = 'Join Request',
 }: {
   relay: string | undefined;
   groupId: string | undefined;
+  text?: string;
+  size?: 'sm' | 'lg' | 'default' | 'icon';
+  variant?: 'default' | 'secondary' | 'link' | 'destructive' | 'outline' | 'ghost';
 }) => {
   const { sendJoinRequest } = useJoinRequestButton(relay, groupId);
 
   return (
-    <Button size="lg" onClick={() => sendJoinRequest()}>
-      Join Request
+    <Button size={size} variant={variant} onClick={() => sendJoinRequest()}>
+      {text}
     </Button>
   );
 };
--- src/features/chats/chat-bottom-bar/components/login-button/index.tsx
+++ src/features/chats/chat-bottom-bar/components/login-button/index.tsx
@@ -2,12 +2,20 @@ import { Button } from '@/shared/components/ui/button';
 
 export type LoginButtonProps = {
   openLoginModal: () => void;
+  text?: string;
+  size?: 'sm' | 'lg' | 'default' | 'icon';
+  variant?: 'default' | 'secondary' | 'link' | 'destructive' | 'outline' | 'ghost';
 };
 
-export const LoginButton = ({ openLoginModal }: LoginButtonProps) => {
+export const LoginButton = ({
+  openLoginModal,
+  size = 'lg',
+  variant = 'default',
+  text = 'To send messages, please login first.',
+}: LoginButtonProps) => {
   return (
-    <Button size="lg" onClick={openLoginModal}>
-      To send messages, please login first.
+    <Button size={size} variant={variant} onClick={openLoginModal}>
+      {text}
     </Button>
   );
 };
