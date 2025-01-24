a0ed5b118f2f309df5c8aabf457ec680ee1d22bf
max21dev <max21dev@gmail.com>
2025-01-24 09:13:10 +0100

    feat: add delete comment functionality to chat thread comments

--- src/features/chats/chat-threads/components/chat-thread-comments/hooks/index.ts
+++ src/features/chats/chat-threads/components/chat-thread-comments/hooks/index.ts
@@ -1,15 +1,59 @@
-import { useGroupThreadComments } from 'nostr-hooks/nip29';
+import { useActiveUser } from 'nostr-hooks';
+import { deleteGroupEvent, useGroupThreadComments } from 'nostr-hooks/nip29';
+import { useState } from 'react';
 
+import { useToast } from '@/shared/components/ui/use-toast';
 import { useActiveGroup, useActiveRelay } from '@/shared/hooks';
 
 export const useChatThreadComments = (parentId: string) => {
   const { activeRelay } = useActiveRelay();
   const { activeGroupId } = useActiveGroup();
+  const { activeUser } = useActiveUser();
+
+  const { toast } = useToast();
 
   const { threadComments, isLoadingThreadComments, hasMoreThreadComments, loadMoreThreadComments } =
     useGroupThreadComments(activeRelay, activeGroupId, {
       byParentId: { parentId, waitForParentId: true },
     });
 
-  return { threadComments, isLoadingThreadComments, hasMoreThreadComments, loadMoreThreadComments };
+  const [deletedComments, setDeletedComments] = useState<string[]>([]);
+
+  const deleteThreadComment = async (commentId: string) => {
+    if (!activeUser || !activeGroupId || !activeRelay) return;
+
+    deleteGroupEvent({
+      relay: activeRelay,
+      groupId: activeGroupId,
+      eventId: commentId,
+      onSuccess: () => {
+        setDeletedComments((prev) => [...prev, commentId]);
+
+        toast({
+          title: 'Success',
+          description: 'Comment deleted successfully',
+          variant: 'default',
+        });
+      },
+      onError() {
+        toast({
+          title: 'Error',
+          description: 'Failed to delete comment',
+          variant: 'destructive',
+        });
+      },
+    });
+  };
+
+  const filteredComments = threadComments?.filter(
+    (comment) => !deletedComments.includes(comment.id),
+  );
+
+  return {
+    threadComments: filteredComments?.length ? filteredComments : undefined,
+    isLoadingThreadComments,
+    hasMoreThreadComments,
+    loadMoreThreadComments,
+    deleteThreadComment,
+  };
 };
