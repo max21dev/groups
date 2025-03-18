0cd60cde7b29fc459a8ea9a29036ea02242769e9
max21dev <max21dev@gmail.com>
2025-03-18 21:17:03 +0100

    refactor: simplify inline code property assignment in rehypeInlineCodeProperty function/ fix lint error

--- src/shared/components/markdown/utils/index.ts
+++ src/shared/components/markdown/utils/index.ts
@@ -2,14 +2,10 @@ import { visit } from 'unist-util-visit';
 
 export function rehypeInlineCodeProperty() {
   return (tree: any) => {
-    visit(tree, 'element', (node: any, index: number | undefined, parent: any) => {
+    visit(tree, 'element', (node: any, _index: number | undefined, parent: any) => {
       if (node.tagName === 'code') {
         node.properties = node.properties || {};
-        if (parent && parent.tagName === 'pre') {
-          node.properties.inline = false;
-        } else {
-          node.properties.inline = true;
-        }
+        node.properties.inline = !(parent && parent.tagName === 'pre');
       }
     });
   };
