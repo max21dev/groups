e44b7caf5ec982f507962e935fb7319ad8f7fd0c
max21dev <max21dev@gmail.com>
2025-03-21 19:15:21 +0100

    feat: add GroupsListMenu component and integrate it into GroupsListItem and PinnedListItem

--- src/features/groups/groups-list-item/index.tsx
+++ src/features/groups/groups-list-item/index.tsx
@@ -1,9 +1,8 @@
 import { memo } from 'react';
 
+import { GroupAvatar, GroupsListMenu } from '@/features/groups';
+
 import { Button } from '@/shared/components/ui/button';
-
-import { GroupAvatar } from '@/features/groups';
-
 import { cn, displayTime, ellipsis } from '@/shared/utils';
 
 import { useGroupsListItem } from './hooks';
@@ -32,49 +31,51 @@ export const GroupsListItem = memo(
     if (!metadata || !showGroup) return null;
 
     return (
-      <Button
-        variant="ghost"
-        size="lg"
-        className={cn(
-          'gap-4 items-center px-2 py-8 cursor-pointer overflow-hidden',
-          isCollapsed && !isMobile ? 'justify-center' : 'justify-start',
-          activeGroupId === groupId && 'bg-accent',
-        )}
-        onClick={() => {
-          setActiveGroupId(groupId);
-          updateLastSeenGroup(activeRelay, groupId);
-        }}
-      >
-        <GroupAvatar relay={activeRelay} groupId={groupId} />
+      <GroupsListMenu groupId={groupId}>
+        <Button
+          variant="ghost"
+          size="lg"
+          className={cn(
+            'w-full gap-4 items-center px-2 py-8 cursor-pointer overflow-hidden',
+            isCollapsed && !isMobile ? 'justify-center' : 'justify-start',
+            activeGroupId === groupId && 'bg-accent',
+          )}
+          onClick={() => {
+            setActiveGroupId(groupId);
+            updateLastSeenGroup(activeRelay, groupId);
+          }}
+        >
+          <GroupAvatar relay={activeRelay} groupId={groupId} />
 
-        {(!isCollapsed || isMobile) && (
-          <div className="flex flex-col items-start w-full min-w-0">
-            <div className="w-full flex items-center">
-              <div className="truncate">{metadata?.name}</div>
+          {(!isCollapsed || isMobile) && (
+            <div className="flex flex-col items-start w-full min-w-0">
+              <div className="w-full flex items-center">
+                <div className="truncate">{metadata?.name}</div>
 
-              {chats && chats.length > 0 && (
-                <div className="ml-auto shrink-0 text-gray-300 text-xs">
-                  {displayTime(chats[0].timestamp)}
-                </div>
-              )}
+                {chats && chats.length > 0 && (
+                  <div className="ml-auto shrink-0 text-gray-300 text-xs">
+                    {displayTime(chats[0].timestamp)}
+                  </div>
+                )}
+              </div>
+
+              <span className="text-gray-400 truncate">
+                {metadata && ellipsis(metadata.about, 20)}{' '}
+              </span>
+
+              <div className="flex items-center w-full justify-between">
+                {chats && chats.length > 0 && (
+                  <span className="text-xs text-gray-300 truncate">
+                    {ellipsis(chats[0].content, 20)}
+                  </span>
+                )}
+
+                {hasNewMessage && <span className="w-2 h-2 bg-blue animate-pulse rounded-full" />}
+              </div>
             </div>
-
-            <span className="text-gray-400 truncate">
-              {metadata && ellipsis(metadata.about, 20)}{' '}
-            </span>
-
-            <div className="flex items-center w-full justify-between">
-              {chats && chats.length > 0 && (
-                <span className="text-xs text-gray-300 truncate">
-                  {ellipsis(chats[0].content, 20)}
-                </span>
-              )}
-
-              {hasNewMessage && <span className="w-2 h-2 bg-blue animate-pulse rounded-full" />}
-            </div>
-          </div>
-        )}
-      </Button>
+          )}
+        </Button>
+      </GroupsListMenu>
     );
   },
   (prevProps, nextProps) => prevProps.groupId === nextProps.groupId,
