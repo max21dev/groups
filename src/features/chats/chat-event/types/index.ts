e3d65bf122bcd1f97a716e48d38a6247ee6e306e
max21dev <max21dev@gmail.com>
2025-03-23 15:15:21 +0100

    feat: add chat message event component and event category types

---
+++ src/features/chats/chat-event/types/index.ts
@@ -0,0 +1,12 @@
+export type EventCategory =
+  | 'follow-set'
+  | 'emoji-set'
+  | 'group'
+  | 'chat-message'
+  | 'note'
+  | 'long-form-content'
+  | 'poll'
+  | 'picture'
+  | 'video'
+  | 'live-stream'
+  | 'highlight';
