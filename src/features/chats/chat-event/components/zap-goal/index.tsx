cd9a11f9323e2111c5fab74a2ef32c6b42ba0f47
max21dev <max21dev@gmail.com>
2025-05-28 14:39:46 +0200

    feat: add ZapGoal component with zap tracking and contributor display

---
+++ src/features/chats/chat-event/components/zap-goal/index.tsx
@@ -0,0 +1,57 @@
+import { NostrEvent } from '@nostr-dev-kit/ndk';
+
+import { ChatContent } from '@/features/chats/chat-list/components/chat-list-item/components';
+import { UserAvatar, UserName } from '@/features/users';
+
+import { Progress } from '@/shared/components/ui/progress';
+import { formatTimestampToDate } from '@/shared/utils';
+
+import { useZapGoal } from './hooks';
+
+export const ZapGoal = ({ event }: { event: NostrEvent }) => {
+  const { categorizedChatContent, closedAt, contributors, isEventPage, percentage, targetMsats } =
+    useZapGoal(event);
+
+  return (
+    <div className="w-full set-max-h overflow-auto flex flex-col gap-2 p-2">
+      <div className="[&_*]:text-base">
+        <ChatContent categorizedChatContent={categorizedChatContent} />
+      </div>
+      <p className="text-sm mt-2">Target: {(targetMsats / 1000).toLocaleString()} sats</p>
+
+      <div className="mt-2 flex items-center gap-2">
+        <Progress value={percentage} className="h-3 rounded-full" />
+        <p className="text-xs inline-block whitespace-nowrap text-muted-foreground text-center">
+          {percentage}%
+        </p>
+      </div>
+
+      {closedAt && (
+        <p className="w-fit text-xs text-muted-foreground">
+          {Date.now() > closedAt * 1000
+            ? 'Goal closed'
+            : `Close at: ${formatTimestampToDate(closedAt)}`}
+        </p>
+      )}
+
+      {isEventPage && contributors.length > 0 && (
+        <div className="mt-4 divide-y divide-primary/20">
+          {contributors.map(({ pubkey, msats, message }, idx) => (
+            <div key={idx} className="flex items-start p-2 gap-2">
+              <div>
+                <div className="flex items-center gap-2 mt-2 [&_span]:w-7 [&_span]:h-7 [&_span]:text-xs">
+                  <UserAvatar pubkey={pubkey} />
+                  <UserName pubkey={pubkey} length={16} className="text-xs" />
+                </div>
+                <p className="my-1 text-sm text-muted-foreground">
+                  {(msats / 1000).toLocaleString()} sats
+                </p>
+                {message && <p className="text-sm">{message}</p>}
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
