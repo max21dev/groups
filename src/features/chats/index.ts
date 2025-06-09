ca49725e8cb9dca6545ba7cf026beeaf587cafd6
max21dev <max21dev@gmail.com>
2025-06-09 11:38:25 +0200

    feat: update CommunityDetails layout for better tag display and export chat-sections

--- src/features/chats/index.ts
+++ src/features/chats/index.ts
@@ -2,5 +2,6 @@ export * from './chat-bottom-bar';
 export * from './chat-event';
 export * from './chat-list';
 export * from './chat-polls';
+export * from './chat-sections';
 export * from './chat-threads';
 export * from './chat-top-bar';
