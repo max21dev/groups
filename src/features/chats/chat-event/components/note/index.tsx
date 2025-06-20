90575c1f04afc8e03dbfc53bf3349c4fb6f74aac
max21dev <max21dev@gmail.com>
2025-06-20 17:11:29 +0200

    refactor: replace ChatContent with RichText in Note component

--- src/features/chats/chat-event/components/note/index.tsx
+++ src/features/chats/chat-event/components/note/index.tsx
@@ -1,14 +1,9 @@
-import { useMemo } from 'react';
-
-import { ChatContent } from '@/features/chats/chat-list/components/chat-list-item/components';
-import { categorizeChatContent } from '@/features/chats/chat-list/components/chat-list-item/utils';
+import { RichText } from '@/shared/components/rich-text';
 
 export const Note = ({ content }: { content: string }) => {
-  const categorizedChatContent = useMemo(() => categorizeChatContent(content || ''), [content]);
-
   return (
     <div className="set-max-h flex flex-col gap-1 p-2 rounded-md overflow-y-auto [overflow-wrap:anywhere]">
-      <ChatContent categorizedChatContent={categorizedChatContent} />
+      <RichText content={content} />
     </div>
   );
 };
