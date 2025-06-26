2693dae876e4ad03a5c12b74090f1c12e7e495ff
max21dev <max21dev@gmail.com>
2025-06-26 15:13:27 +0200

    refactor: simplify GroupsSearch component by removing unused imports and restructuring code

--- src/features/groups/groups-search/index.tsx
+++ src/features/groups/groups-search/index.tsx
@@ -1,28 +1,26 @@
-import { SearchIcon } from 'lucide-react';
-
 import { useAllGroupsMetadataRecords } from 'nostr-hooks/nip29';
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { Link } from 'react-router-dom';
 
 import { GroupAvatar } from '@/features/groups/group-avatar';
-import { useHomePage } from '@/pages/home/hooks';
 
 import { Search } from '@/shared/components/search';
 import { useSearch } from '@/shared/components/search/hooks';
 import { Spinner } from '@/shared/components/spinner';
-import { Button } from '@/shared/components/ui/button';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/shared/components/ui/dialog';
 import { useActiveRelay } from '@/shared/hooks';
+import { ellipsis } from '@/shared/utils';
 
-import { cn, ellipsis } from '@/shared/utils';
-
-export const GroupsSearch = () => {
-  const [isOpen, setIsOpen] = useState(false);
+export const GroupsSearch = ({
+  isOpen,
+  setIsOpen,
+}: {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+}) => {
   const { activeRelay } = useActiveRelay();
 
   const { metadataRecords, isLoadingMetadata } = useAllGroupsMetadataRecords(activeRelay);
-
-  const { isCollapsed, isMobile } = useHomePage();
 
   const groupsListData = useMemo(
     () =>
@@ -43,69 +41,58 @@ export const GroupsSearch = () => {
   }
 
   return (
-    <>
-      <Button
-        variant="outline"
-        size="icon"
-        className={cn('p-2', isCollapsed && !isMobile && 'w-full')}
-        onClick={() => setIsOpen(true)}
-      >
-        <SearchIcon className="cursor-pointer" />
-      </Button>
+    <Dialog
+      open={isOpen}
+      onOpenChange={() => {
+        setIsOpen(false);
+        setSearchTerm('');
+      }}
+    >
+      <DialogContent className="sm:max-w-[425px] top-44 h-80 flex flex-col">
+        <DialogHeader>
+          <DialogTitle className="mt-2">
+            <Search
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+              placeholder="Search Groups"
+            />
+          </DialogTitle>
+        </DialogHeader>
 
-      <Dialog
-        open={isOpen}
-        onOpenChange={() => {
-          setIsOpen(false);
-          setSearchTerm('');
-        }}
-      >
-        <DialogContent className="sm:max-w-[425px] top-44 h-80 flex flex-col">
-          <DialogHeader>
-            <DialogTitle className="mt-2">
-              <Search
-                searchTerm={searchTerm}
-                setSearchTerm={setSearchTerm}
-                placeholder="Search Groups"
-              />
-            </DialogTitle>
-          </DialogHeader>
-
-          {!searchTerm.length ? (
-            <p className="text-center text-muted-foreground">
-              Search Groups in {activeRelay.replace('wss://', '')}
-            </p>
-          ) : (
-            <>
-              {isLoadingMetadata ? (
-                <Spinner />
-              ) : (
-                <div className="overflow-auto flex flex-col justify-start gap-1">
-                  {filteredData.map((group) => (
-                    <Link
-                      to={`/?relay=${activeRelay}&groupId=${group.id}`}
-                      key={group.id}
-                      className="flex items-center gap-2 p-2 rounded-md cursor-pointer hover:bg-accent"
-                      onClick={() => {
-                        setIsOpen(false);
-                        setSearchTerm('');
-                      }}
-                    >
-                      <GroupAvatar relay={activeRelay} groupId={group.id} />
-                      <div className="flex flex-col gap-1">
-                        <p className="text-sm font-semibold truncate">{ellipsis(group.name, 20)}</p>
-                        <p className="text-xs truncate text-muted-foreground">
-                          {ellipsis(group.about, 20)}
-                        </p>
-                      </div>
-                    </Link>
-                  ))}
-                </div>
-              )}
-            </>
-          )}
-        </DialogContent>
-      </Dialog>
-    </>
+        {!searchTerm.length ? (
+          <p className="text-center text-muted-foreground">
+            Search Groups in {activeRelay.replace('wss://', '')}
+          </p>
+        ) : (
+          <>
+            {isLoadingMetadata ? (
+              <Spinner />
+            ) : (
+              <div className="overflow-auto flex flex-col justify-start gap-1">
+                {filteredData.map((group) => (
+                  <Link
+                    to={`/?relay=${activeRelay}&groupId=${group.id}`}
+                    key={group.id}
+                    className="flex items-center gap-2 p-2 rounded-md cursor-pointer hover:bg-accent"
+                    onClick={() => {
+                      setIsOpen(false);
+                      setSearchTerm('');
+                    }}
+                  >
+                    <GroupAvatar relay={activeRelay} groupId={group.id} />
+                    <div className="flex flex-col gap-1">
+                      <p className="text-sm font-semibold truncate">{ellipsis(group.name, 20)}</p>
+                      <p className="text-xs truncate text-muted-foreground">
+                        {ellipsis(group.about, 20)}
+                      </p>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            )}
+          </>
+        )}
+      </DialogContent>
+    </Dialog>
   );
 };
