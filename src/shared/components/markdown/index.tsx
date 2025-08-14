6e539d7f687e6c794423516839c6a6bf97ffe191
max21dev <max21dev@gmail.com>
2025-08-14 09:49:45 +0300

    feat: extend Markdown component to support emoji rendering with EmojiRenderer

--- src/shared/components/markdown/index.tsx
+++ src/shared/components/markdown/index.tsx
@@ -1,14 +1,24 @@
+import { NDKTag } from '@nostr-dev-kit/ndk';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
 import { UserMention } from '@/features/users/user-mention';
 
+import { EmojiRenderer } from '@/shared/components/emoji-renderer';
 import { cn } from '@/shared/utils';
 
 import { Code } from './components/code';
 import { rehypeInlineCodeProperty } from './utils';
 
-export const Markdown = ({ content, className }: { content: string; className?: string }) => {
+export const Markdown = ({
+  content,
+  className,
+  emojiTags,
+}: {
+  content: string;
+  className?: string;
+  emojiTags?: NDKTag[];
+}) => {
   return (
     <div className={cn('prose prose-invert max-w-full relative z-0', className)}>
       <ReactMarkdown
@@ -40,7 +50,20 @@ export const Markdown = ({ content, className }: { content: string; className?: 
             </blockquote>
           ),
           code: Code,
-          img: ({ node, ...props }) => <img {...props} className="rounded-md max-w-full" />,
+          img: ({ node, ...props }) => {
+            if (props.src?.startsWith('/emoji/')) {
+              const code = decodeURIComponent(props.src.replace('/emoji/', ''));
+              const isBig = (props.title || '').toLowerCase() === 'big';
+              return (
+                <EmojiRenderer
+                  shortcode={code}
+                  tags={emojiTags}
+                  className={isBig ? 'size-12 sm:size-28' : ''}
+                />
+              );
+            }
+            return <img {...props} className="rounded-md max-w-full" />;
+          },
           hr: () => <hr className="border-t border-primary/50 my-6" />,
         }}
       >
